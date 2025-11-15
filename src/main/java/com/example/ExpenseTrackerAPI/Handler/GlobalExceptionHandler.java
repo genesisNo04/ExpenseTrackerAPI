85293e755ae5851398e266db58ab4f64e69d5514
namnguyen <namhoanghainguyen@gmail.com>
2025-11-15 10:51:33 -0700

    Finish up the testing

--- src/main/java/com/example/ExpenseTrackerAPI/Handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/ExpenseTrackerAPI/Handler/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import com.example.ExpenseTrackerAPI.Exception.ResourceNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
--- src/test/java/com/example/ExpenseTrackerAPI/IntegrationTest/ExpenseIntegrationTesting.java
+++ src/test/java/com/example/ExpenseTrackerAPI/IntegrationTest/ExpenseIntegrationTesting.java
@@ -1,16 +1,17 @@
 package com.example.ExpenseTrackerAPI.IntegrationTest;
 
-import com.example.ExpenseTrackerAPI.Entity.Expense;
 import com.example.ExpenseTrackerAPI.Enum.Category;
 import com.example.ExpenseTrackerAPI.Repository.AuthUserRepository;
 import com.example.ExpenseTrackerAPI.Repository.ExpenseRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,7 +19,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import java.time.LocalDateTime;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -27,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ExpenseIntegrationTesting {
 
     @Autowired
@@ -48,12 +49,28 @@ public class ExpenseIntegrationTesting {
                 """, username, password, email);
     }
 
-    private String expenseRequestBody(String title, String description, Category category, double amount, LocalDateTime createdTime) {
+    private final String  UNAUTHORIZED_ERROR_MESSAGE = "You are not authorized to access this expense. This expense does not belong to you.";
+    private final String  RESOURCE_NOT_FOUND_ERROR_MESSAGE = "Expense not found.";
+    private final String  NO_TOKEN_ERROR_MESSAGE = "Full authentication is required to access this resource";
+
+    private String expenseRequestBody(String title, String description, Category category, Double amount, LocalDateTime createdTime) {
         StringBuilder sb = new StringBuilder("{");
-        sb.append("\"title\": \"").append(title).append("\",");
-        sb.append("\"description\": \"").append(description).append("\",");
-        sb.append("\"category\": \"").append(category).append("\",");
-        sb.append("\"amount\": \"").append(amount).append("\",");
+
+        if (title != null) {
+            sb.append("\"title\": \"").append(title).append("\",");
+        }
+
+        if (description != null) {
+            sb.append("\"description\": \"").append(description).append("\",");
+        }
+
+        if (category != null) {
+            sb.append("\"category\": \"").append(category).append("\",");
+        }
+
+        if (amount != null) {
+            sb.append("\"amount\": \"").append(amount).append("\",");
+        }
 
         if (createdTime != null) {
             sb.append("\"createdTime\": \"").append(createdTime).append("\",");
@@ -90,6 +107,12 @@ public class ExpenseIntegrationTesting {
                 .content(expenseRequestBody(title, description, category, amount, createdTime)));
     }
 
+    @BeforeEach
+    void cleanDatabase() {
+        expenseRepository.deleteAll();
+        authUserRepository.deleteAll();
+    }
+
     @Test
     void shouldReturnExpenseWhenCreate() throws Exception {
         String token = registerUser("user", "user", "user@gmail.com");
@@ -106,6 +129,18 @@ public class ExpenseIntegrationTesting {
     }
 
     @Test
+    void shouldThrowErrorWithoutSendingToken() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        mockMvc.perform(post("/v1/expense")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(expenseRequestBody("expense 1", "expense desc 1", Category.GROCERIES, 101.20, null)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value(NO_TOKEN_ERROR_MESSAGE));
+    }
+
+
+    @Test
     void shouldThrowErrorWithInvalidToken() throws Exception {
         String invalidJwt =
                 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbnZhbGlkVXNlciIsImlhdCI6MTcwMDAwMDAwMH0.Ww9zVkoGxO8DvyjP6TfGsDz3jHK5xL0F4nBM8h8fr9Y";
@@ -120,15 +155,10 @@ public class ExpenseIntegrationTesting {
     }
 
     @Test
-    void ShouldReturnExpenseWithIdWithValidToken() throws Exception {
+    void shouldReturnExpenseWithIdWithValidToken() throws Exception {
         String token = registerUser("user", "user", "user@gmail.com");
 
-        mockMvc.perform(post("/v1/expense")
-                        .header("Authorization", "Bearer " + token)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(expenseRequestBody("expense 1", "expense desc 1", Category.GROCERIES, 101.20, null)))
-                .andExpect(status().isCreated())
-                .andReturn();
+        createExpense("expense 1", "expense desc 1", Category.GROCERIES, 101.20, token, null);
 
         long id = 1;
 
@@ -139,6 +169,29 @@ public class ExpenseIntegrationTesting {
                 .andExpect(jsonPath("$.description").value("expense desc 1"))
                 .andExpect(jsonPath("$.category").value("GROCERIES"))
                 .andExpect(jsonPath("$.amount").value(101.20));
+    }
+
+    @Test
+    void ShouldThrowNoResourceFoundWithIdNotExist() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        long id = 1;
+
+        mockMvc.perform(get("/v1/expense/" + id)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isFound())
+                .andExpect(jsonPath("$.error").value(RESOURCE_NOT_FOUND_ERROR_MESSAGE));
+    }
+
+    @Test
+    void ShouldThrowUnauthorizedWithoutSendingTokenWithSearchWithId() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        long id = 1;
+
+        mockMvc.perform(get("/v1/expense/" + id))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value(NO_TOKEN_ERROR_MESSAGE));
     }
 
     @Test
@@ -159,7 +212,7 @@ public class ExpenseIntegrationTesting {
                         .header("Authorization", "Bearer " + token1))
                 .andDo(print())
                 .andExpect(status().isForbidden())
-                .andExpect(jsonPath("$.error").value("You are not authorized to access this expense. This expense does not belong to you."));
+                .andExpect(jsonPath("$.error").value(UNAUTHORIZED_ERROR_MESSAGE));
     }
 
     @Test
@@ -188,7 +241,7 @@ public class ExpenseIntegrationTesting {
     }
 
     @Test
-    void shouldThrowForbiddenWithDifferentUser() throws Exception {
+    void shouldNotReturnExpenseOfDifferentUser() throws Exception {
         String token = registerUser("user", "user", "user@gmail.com");
         String token1 = registerUser("user1", "user1", "user1@gmail.com");
         createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 11, 14, 16, 30, 30, 0));
@@ -197,8 +250,20 @@ public class ExpenseIntegrationTesting {
 
         mockMvc.perform(get("/v1/expense")
                         .header("Authorization", "Bearer " + token1))
-                .andExpect(status().isForbidden())
-                .andExpect(jsonPath("error").value("You are not authorized to access this expense. This expense does not belong to you."));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    void shouldThrowUnauthorizedWithoutSendingTokenWithSearchAll() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 11, 14, 16, 30, 30, 0));
+        createExpense("expense2", "expense description 2", Category.ELECTRONIC, 358.6, token, LocalDateTime.of(2025, 12, 14, 16, 30, 30, 0));
+        createExpense("expense3", "expense description 3", Category.LEISURE, 1503.6, token, LocalDateTime.of(2025, 10, 14, 16, 30, 30, 0));
+
+        mockMvc.perform(get("/v1/expense"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value(NO_TOKEN_ERROR_MESSAGE));
     }
 
     @Test
@@ -258,6 +323,217 @@ public class ExpenseIntegrationTesting {
                 .andExpect(jsonPath("$[1].description").value("expense description 2"))
                 .andExpect(jsonPath("$[1].category").value("ELECTRONIC"))
                 .andExpect(jsonPath("$[1].amount").value(358.6));
+    }
+
+    @Test
+    void shouldReturnUpdatedExpense() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 10, 12, 16, 30, 30, 0));
+
+        String updateExpense = expenseRequestBody("expense1 update", "expense description 1 update", Category.CLOTHING, 200.50, null);
+
+        mockMvc.perform(put("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("expense1 update"))
+                .andExpect(jsonPath("$.description").value("expense description 1 update"))
+                .andExpect(jsonPath("$.category").value("CLOTHING"))
+                .andExpect(jsonPath("$.amount").value(200.50));
+    }
+
+    @Test
+    void shouldThrowForbiddenUpdateExpenseWithUnauthorizedUser() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        String token1 = registerUser("user1", "user1", "user1@gmail.com");
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 10, 12, 16, 30, 30, 0));
+
+        String updateExpense = expenseRequestBody("expense1 update", "expense description 1 update", Category.CLOTHING, 200.50, null);
+
+        mockMvc.perform(put("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.error").value(UNAUTHORIZED_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldThrowUnauthorizedUpdateExpenseWithoutSendingToken() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 10, 12, 16, 30, 30, 0));
+
+        String updateExpense = expenseRequestBody("expense1 update", "expense description 1 update", Category.CLOTHING, 200.50, null);
+
+        mockMvc.perform(put("/v1/expense/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value(NO_TOKEN_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldThrowResourceNotFoundExpenseWithIdNotExists() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        String updateExpense = expenseRequestBody("expense1 update", "expense description 1 update", Category.CLOTHING, 200.50, null);
+
+        mockMvc.perform(put("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error").value(RESOURCE_NOT_FOUND_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldReturnPatchExpense() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 10, 12, 16, 30, 30, 0));
+
+        String updateExpense = expenseRequestBody("expense1 update", null, Category.CLOTHING, null, null);
+
+        mockMvc.perform(patch("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("expense1 update"))
+                .andExpect(jsonPath("$.description").value("expense description 1"))
+                .andExpect(jsonPath("$.category").value("CLOTHING"))
+                .andExpect(jsonPath("$.amount").value(153.6));
+    }
+
+    @Test
+    void shouldThrowForbiddenPatchExpenseWithUnauthorizedUser() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        String token1 = registerUser("user1", "user1", "user1@gmail.com");
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 10, 12, 16, 30, 30, 0));
+
+        String updateExpense = expenseRequestBody(null, "expense description 1 update", null, 200.50, null);
+
+        mockMvc.perform(patch("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.error").value(UNAUTHORIZED_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldThrowUnauthorizedPatchExpenseWithoutSendingToken() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, LocalDateTime.of(2025, 10, 12, 16, 30, 30, 0));
+
+        String updateExpense = expenseRequestBody("expense1 update", "expense description 1 update", Category.CLOTHING, 200.50, null);
+
+        mockMvc.perform(patch("/v1/expense/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value(NO_TOKEN_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldThrowResourceNotFoundPatchExpenseWithIdNotExists() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        String updateExpense = expenseRequestBody("expense1 update", null, null, null, null);
+
+        mockMvc.perform(patch("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateExpense))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error").value(RESOURCE_NOT_FOUND_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldDeleteExpenseWithId() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense2", "expense description 2", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense3", "expense description 3", Category.GROCERIES, 153.6, token, null);
+
+
+        mockMvc.perform(delete("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get("/v1/expense")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].title").value("expense2"))
+                .andExpect(jsonPath("$[1].title").value("expense3"));
+    }
+
+    @Test
+    void shouldThrowUnauthorizedWithoutSendingToken() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, null);
+
+        mockMvc.perform(delete("/v1/expense/1"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value(NO_TOKEN_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldThrowForbiddenExceptionWithDifferentUserToken() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        String token1 = registerUser("user1", "use1r", "user1@gmail.com");
+
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, null);
+
+
+        mockMvc.perform(delete("/v1/expense/1")
+                        .header("Authorization", "Bearer " + token1))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.error").value(UNAUTHORIZED_ERROR_MESSAGE));
+    }
+
+    @Test
+    void shouldReturnExpenseSummary() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense2", "expense description 2", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense3", "expense description 3", Category.GROCERIES, 153.6, token, null);
+
+        mockMvc.perform(get("/v1/expense/summary")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.summary").value(460.8));
+    }
+
+    @Test
+    void shouldNotReturnExpenseSummaryOfAnotherUser() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+        String token1 = registerUser("user1", "user1", "user1@gmail.com");
+
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense2", "expense description 2", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense3", "expense description 3", Category.GROCERIES, 153.6, token, null);
+
+        mockMvc.perform(get("/v1/expense/summary")
+                        .header("Authorization", "Bearer " + token1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.summary").value(0.0));
+    }
+
+    @Test
+    void shouldThrowUnauthorizedWithoutSendingTokenForSummary() throws Exception {
+        String token = registerUser("user", "user", "user@gmail.com");
+
+        createExpense("expense1", "expense description 1", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense2", "expense description 2", Category.GROCERIES, 153.6, token, null);
+        createExpense("expense3", "expense description 3", Category.GROCERIES, 153.6, token, null);
+
+        mockMvc.perform(get("/v1/expense/summary"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value(NO_TOKEN_ERROR_MESSAGE));
     }
 
 }
